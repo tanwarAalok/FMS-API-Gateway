e22493dac0acdd645ef11394ccfd298086ed36a2
Aalok Tanwar <aaloktanwar88@gmail.com>
2023-06-06 12:29:59 +0530

    added rate limiter and proxy

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -7,4 +7,6 @@ module.exports = {
   SALT_ROUNDS: process.env.SALT_ROUNDS,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRE: process.env.JWT_EXPIRE,
+  BOOKING_SERVICE_URL: process.env.BOOKING_SERVICE_URL,
+  FLIGHT_SERVICE_URL: process.env.FLIGHT_SERVICE_URL,
 };
